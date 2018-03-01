738a332364065e0fedd208e4839afb942eb6f4b3
Morgan Fudge <mfudge92@gmail.com>
2018-03-01 14:52:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 currentUser = 'currentUser'
 
-welcomeMessage =  "Welcome to Flatbook, ${currentUser}!"
+welcomeMessage =  `Welcome to Flatbook, ${currentUser}!`
 
 excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
